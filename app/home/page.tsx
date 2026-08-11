4288baa1ba4705f70fd450338ea4bf10f6a2357c
phattin <tin220905@gmail.com>
2026-08-11 14:45:41 +0700

    refactor: clean up layout and remove unused font imports; add initial Home and Login components

---
+++ app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <p>Welcome to the Home Page</p>
+        </div>
+    );
+}
